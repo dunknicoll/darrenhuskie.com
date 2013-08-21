96e984d3b9d7dce33cc70e023f1a1e7af09f038d
Darren Huskie <darren@darrenhuskie.com>
2013-08-21 23:32:42 +0100

    Preliminary History API work

--- assets/js/build/build.js
+++ assets/js/build/build.js
@@ -7,6 +7,18 @@ u[o]&&(delete u[o],c?delete n[l]:typeof n.removeAttribute!==i?n.removeAttribute(
 
 var DH = {
 
+	doHistoryAPI: function() {
+		// History API script
+
+		// Check whether the browser supports the History API
+		var supported = supportsHistoryAPI();
+
+		if(supported) {
+			// Add click event handlers to our links
+			addHistoryAPIClickHandlers();
+		}
+	},
+
     applyAnimations: function() {
         // Apply animations to elements
     }
@@ -15,5 +27,35 @@ var DH = {
 
 $(function()
 {
+	DH.doHistoryAPI();
     DH.applyAnimations();
 });
+
+// Static functions
+
+function supportsHistoryAPI() {
+	return !!(window.history && history.pushState);
+}
+
+function addHistoryAPIClickHandlers() {
+	$('a').click(function(event) {
+		event.preventDefault();
+		loadContent($(this).attr('href'));
+    	//history.pushState(null, null, link.href);
+  	});
+}
+
+function loadContent(link) {
+	// Prepare the URL parameter
+	var content_url = '/content/' + link.substring(12);
+	content_url = content_url.replace('.html', '.json');
+
+	// Load the desired content through AJAX
+	$.ajax({
+  		dataType: 'json',
+  		url: content_url,
+  		success: function() {
+  			console.log('Boom');
+  		}
+	});
+}
