6d5f764dcb31f1959b9ad42b9b01974b166e9a1a
Darren Huskie <darren@darrenhuskie.com>
2013-09-18 22:12:50 +0100

    Current build

--- _plugins/generate_json.rb
+++ _plugins/generate_json.rb
@@ -1,6 +1,17 @@
 module Jekyll
   require 'json'
 
+  class JsonFile < StaticFile
+    def write(dest)
+      begin
+        super(dest)
+      rescue
+      end
+
+      true
+    end
+  end
+
   class JSONGenerator < Generator
     safe true
     priority :low
@@ -9,33 +20,28 @@ module Jekyll
       # Converter for .md > .html
       converter = site.getConverterImpl(Jekyll::Converters::Markdown)
 
+      # Start building the path
+      path = "_site/content"
+      hash = Hash.new("0")
+
       # Iterate over all posts
       site.posts.each do |post|
 
         # Encode the HTML to JSON
-        hash = { "content" => converter.convert(post.content)}
-        title = post.title.downcase.tr(' ', '-').delete("’!")
-
-        # Start building the path
-        path = "_site/"
-
-        # Add categories to path if they exist
-        if (post.data['categories'].class == String)
-          path << post.data['categories'].tr(' ', '/')
-        elsif (post.data['categories'].class == Array)
-          path <<  post.data['categories'].join('/')
-        end
-
-        # Add the sanitized post title to complete the path
-        path << "/#{title}"
-
-        # Create the directories from the path
-        FileUtils.mkpath(path) unless File.exists?(path)
-
-        # Create the JSON file and inject the data
-        f = File.new("#{path}/raw.json", "w+")
-        f.puts JSON.generate(hash)
+        link = post.url
+        hash[link] = {"title" => post.title, "content" => converter.convert(post.content)}
       end
+
+	  	# hash = [hashstring.join(',')]
+
+	    # Create the directories from the path
+	    FileUtils.mkpath(path) unless File.exists?(path)
+
+	    # Create the JSON file and inject the data
+	    f = File.new("#{path}/content.json", "w+")
+	    f.puts JSON.generate(hash)
+
+	    site.static_files << Jekyll::JsonFile.new(site, site.dest, "content", "content.json")
 
     end
 
